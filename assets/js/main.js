a4f5413ce301d16842d99090758a9a37b8e0afa4
Cocovista <63193976+Cocovista@users.noreply.github.com>
2020-04-24 17:30:22 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+
+
+
+
     $(function() {
         /**
         * Smooth scrolling to page anchor on click
