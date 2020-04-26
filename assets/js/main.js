30374db41845b93c269d979943060692418d5e6e
Cocovista <63193976+Cocovista@users.noreply.github.com>
2020-04-26 18:16:05 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,6 @@
 
 /** Formulaire 
 **/
-$("#contact").submit(function(e) {
-  e.preventDefault();
-
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Thank you!");
-  });
-});
 
 
 /** Je trouve ma classe 
