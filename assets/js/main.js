0216048a55dc5b59b27222cc5f55601d29c3292e
Cocovista <63193976+Cocovista@users.noreply.github.com>
2020-04-29 11:05:44 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,6 @@
 /** Formulaire 
 **/
 
-/** Je trouve ma classe index général
-**/
-document.getElementById("jetrouvemaclasse").onclick = function() {jetrouvemaclasse()};
-
-function jetrouvemaclasse() {
-  document.getElementById("jetrouvemaclasse").innerHTML = "Cherche dans Collège ou Lycée";
-};
-
-
-
 $(function() {
         /**
         * Smooth scrolling to page anchor on click
@@ -94,5 +84,16 @@ $(function() {
 				});
 
 })(jQuery);
+
+
+/** Je trouve ma classe index général
+**/
+document.getElementById("jetrouvemaclasse").onclick = function() {jetrouvemaclasse()};
+
+function jetrouvemaclasse() {
+  document.getElementById("jetrouvemaclasse").innerHTML = "Cherche dans Collège ou Lycée";
+};
+
+
 
 
